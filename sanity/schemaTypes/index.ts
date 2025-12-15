978298be300dfc4fdb74ea2a2b0a8b091bc43456
Andrew Reddin <areddin409@gmail.com>
2025-12-14 20:01:44 -0600

    feat: update Sanity schema to include order, category, and product types; add sample data for categories and products

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,9 @@
 import { type SchemaTypeDefinition } from "sanity"
 import { customerType } from "./customerTypes"
+import { orderType } from "./orderType"
+import { categoryType } from "./categoryType"
+import { productType } from "./productType"
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [customerType]
+  types: [customerType, orderType, categoryType, productType]
 }
