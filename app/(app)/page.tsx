9d7cea278fd999ff28b68072a0ea3911fe54a4f3
Andrew Reddin <areddin409@gmail.com>
2025-12-14 20:50:57 -0600

    feat: add Sanity queries for categories, customers, orders, products, and stats; implement live updates in layout

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,6 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { sanityFetch } from "@/sanity/lib/live"
+import { ALL_CATEGORIES_QUERY } from "@/sanity/queries/categories"
 
-export default function Home() {
+export default async function Home() {
+  //fetch categories for filter sidebar
+  const { data: categories } = await sanityFetch({
+    query: ALL_CATEGORIES_QUERY
+  })
   return (
     <div className="">
       {/* Feature Products Carousel */}
